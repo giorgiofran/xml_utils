a7ca74e3a18c0afb058e4e4821b14a6a9a1a1ffc
Giorgio <giorgiofran@gmail.com>
2021-09-11 10:17:35 +0200

    small changes

--- lib/xml_utils.dart
+++ lib/xml_utils.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library xml_utils;
 
 export 'src/xml_utils_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
